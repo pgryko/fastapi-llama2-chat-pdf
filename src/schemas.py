27fb03f4ff69077b50760f5296bae4e60e7556b2
Piotr Gryko <piotr.gryko@gmail.com>
2023-08-06 12:50:58 +0200

    Fix: added gitlab caching and formated file

--- src/schemas.py
+++ src/schemas.py
@@ -1,12 +1,15 @@
 from pydantic import BaseModel
 from typing import List
 
+
 class Message(BaseModel):
     role: str
     content: str
 
+
 class ChatInput(BaseModel):
     messages: List[Message]
+
 
 class ChatOutput(BaseModel):
     message: Message
